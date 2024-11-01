0231295ce8e2713902b8115df7c36221d65b5bcf
M. Andrés Piana <153022165+MateoAPiana@users.noreply.github.com>
2024-11-01 09:12:41 -0300

    style and text to the home

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,6 +1,18 @@
+import "./home.css"
 export function Home() {
   return (
     <div>
+      Simplify Your Links!
+      <br />
+      <p>Turn long links into short, easy-to-share URLs. With our URL shortener, you can customize, manage, and analyze your links all in one place. Perfect for social media, marketing, and anywhere you need a clean, direct link.</p>
+
+      <h2 className="subtitle">Why Choose Us?</h2>
+      <ul className="listHome">
+        <li className="listItem">Customizable Links: Give your URLs a unique touch.</li>
+        <li className="listItem">Real-Time Analytics: Track link performance with precise metrics.</li>
+        <li className="listItem">Completely Free: No hassle—start shortening immediately.</li>
+        Get started now and keep your links simple and effective. Your shorter URL is just a click away!
+      </ul>
     </div>
   )
 }
